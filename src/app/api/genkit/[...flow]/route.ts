265f139954e3856519948d1654cda2bc79c170f5
NadaNagdy <141024996+NadaNagdy@users.noreply.github.com>
2026-01-12 22:05:33 +0200

    Update route.ts

--- src/app/api/genkit/[...flow]/route.ts
+++ src/app/api/genkit/[...flow]/route.ts
@@ -1,8 +1,8 @@
+import { genkit } from 'genkit';
 import defineNextHandler from '@genkit-ai/next';
 import '@/ai/dev';
 
-const handler = defineNextHandler({
-  flows: [],
-});
+const ai = genkit({});
+const handler = defineNextHandler(ai);
 
 export { handler as GET, handler as POST };
